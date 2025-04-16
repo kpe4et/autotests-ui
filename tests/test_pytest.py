524c1fd5e0862607defd1324ee5302ed19026657
kpe4et <kpe4ets@gmail.com>
2025-04-16 23:04:53 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,23 @@
+def test_first_try():
+    print("Hello World!")
+
+
+class TestUserLogin:
+
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+
+def test_assert_positive_case():  # Новый тест, которые проверяет положительный кейс
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
+    assert (3 + 3) == 6  # Ожидается, что тест пройдет
+    assert (4 + 4) == 8  # Ожидается, что тест пройдет
+
+
+def test_assert_negative_case():  # Новый тест, которые проверяет негативный кейс
+    assert (2 + 2) == 5, "2 + 2 != 5" # Тут должна быть ошибка
+
+
