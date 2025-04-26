6425462cb385a1fac55cfef2fbce191b39663067
kpe4et <kpe4ets@gmail.com>
2025-04-27 01:31:47 +0300

    Параметризация Pytest.Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,22 +1,26 @@
 from playwright.sync_api import expect, Page
 import pytest
 
-
+@pytest.mark.parametrize("email, password", 
+        [("user.name@gmail.com", "password"), 
+        ("user.name@gmail.com", "  "), 
+        ("  ", "password")]
+        )
 @pytest.mark.regression
 @pytest.mark.authorization
 # Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+def test_wrong_email_or_password_authorization(chromium_page: Page, email, password):
 
     # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
     
     # Находим поля ввода для электронной почты и заполняем его
     email_input = chromium_page.get_by_test_id("login-form-email-input").locator('input')
-    email_input.fill('user@gmail.com') 
+    email_input.fill(email) 
 
     # Находим поле ввода для пароля и заполняем его
     password_input = chromium_page.get_by_test_id("login-form-password-input").locator('input')
-    password_input.fill('password')
+    password_input.fill(password)
 
     # Находим кнопку "Login" и нажимаем ее
     login_button = chromium_page.get_by_test_id("login-page-login-button")
