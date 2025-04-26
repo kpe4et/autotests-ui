d8af212c1c4b3de5191185439965cf4b891942e4
kpe4et <kpe4ets@gmail.com>
2025-04-26 22:15:10 +0300

    Идентификаторы

--- tests/test_parametrization.py
+++ tests/test_parametrization.py
@@ -35,3 +35,19 @@ class TestOperations:
 
     def test_user_without_operations(self, user: str):
         print(f"User without operations: {user}")
+
+
+users = {
+    "+70000000011": "User with money on bank account",
+    "+70000000022": "User without money on bank account",
+    "+70000000033": "User with operations on bank account"
+}
+
+
+@pytest.mark.parametrize(
+    "phone_number",
+    users.keys(),
+    ids=lambda phone_number: f"{phone_number}: {users[phone_number]}"
+)
+def test_identifiers(phone_number: str):
+    pass
