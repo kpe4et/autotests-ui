8c49584b1a7156385a2d179c84e9d2a35709e6a4
kpe4et <kpe4ets@gmail.com>
2025-04-30 01:39:15 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -3,23 +3,23 @@ import pytest
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
-# @pytest.mark.courses
-# @pytest.mark.regression
-# def test_empty_courses_list(chromium_page_with_state: Page):
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(chromium_page_with_state: Page):
         
-#         chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-#         courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-#         expect(courses_title).to_have_text("Courses")
+        courses_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+        expect(courses_title).to_have_text("Courses")
 
-#         course_results_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-#         expect(course_results_icon).to_be_visible()
+        course_results_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+        expect(course_results_icon).to_be_visible()
 
-#         course_results_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-#         expect(course_results_title).to_have_text("There is no results")
+        course_results_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+        expect(course_results_title).to_have_text("There is no results")
 
-#         course_results_info = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-#         expect(course_results_info).to_have_text("Results from the load test pipeline will be displayed here")
+        course_results_info = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
+        expect(course_results_info).to_have_text("Results from the load test pipeline will be displayed here")
 
 
 @pytest.mark.courses
