a50f9460aecf72c9ce2e69def81d6271b28c0db3
kpe4et <kpe4ets@gmail.com>
2025-05-02 10:56:03 +0300

    Реализация и использование компонента

--- components/courses/create_course_exercise_form_component.py
+++ components/courses/create_course_exercise_form_component.py
@@ -20,12 +20,9 @@ class CourseExercisesFormComponent(BaseComponent):
 
     def check_visible(self, index: int, title: str, description: str):
         subtitle = self.page.get_by_test_id(f'create-course-exercise-{index}-box-toolbar-subtitle-text')
-        title_input = self.page.get_by_test_id(
-            f'create-course-exercise-form-title-{index}-input'
-        )
-        description_input = self.page.get_by_test_id(
-            f'create-course-exercise-form-description-{index}-input'
-        )
+        title_input = self.page.get_by_test_id(f'create-course-exercise-form-title-{index}-input')
+        description_input = self.page.get_by_test_id(f'create-course-exercise-form-description-{index}-input')
+        
         expect(subtitle).to_be_visible()
         expect(subtitle).to_have_text(f"#{index + 1} Exercise")
 
