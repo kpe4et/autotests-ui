d6f1306e7f7d073a984b9614b7b24959c08de48c
kpe4et <kpe4ets@gmail.com>
2025-05-04 13:21:13 +0300

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- elements/textarea.py
+++ elements/textarea.py
@@ -2,7 +2,7 @@ from elements.base_element import BaseElement
 
 from playwright.sync_api import expect
 
-class Text(BaseElement):
+class TextArea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs):
         return super().get_locator(nth, **kwargs).locator('textarea').first
     
@@ -10,6 +10,6 @@ class Text(BaseElement):
         locator = self.get_locator(nth, **kwargs)
         locator.fill(value)
 
-    def check_have_text(self, value: str, nth: int = 0, **kwargs):
+    def check_have_value(self, value: str, nth: int = 0, **kwargs):
         locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_value(value)
