eb0289afd32e907701d8343299caf3afe1315e8b
kpe4et <kpe4ets@gmail.com>
2025-05-10 16:54:14 +0300

    Добавление видео в Allure отчет. Рефакторинг фикстур chromium_page и chromium_page_with_state

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,25 +4,12 @@ from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно1
 def chromium_page(request: SubRequest,playwright: Playwright) -> Page:
-    # Запускаем браузер
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-
-    # Передаем страницу для использования в тесте
-    yield browser.new_page()
-
-    # Закрываем браузер после выполнения тестов. request.node.name содержит название текущего автотеста
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', attachment_type='application/zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 # Установка состояния браузера в качестве фикстуры
 @pytest.fixture(scope='session')
@@ -46,15 +33,4 @@ def initialize_browser_state(playwright: Playwright) -> None:
 # Фикстура для запуска тестов с сохраненным состоянием браузера
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json", record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-    
-    yield context.new_page()
-    
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', attachment_type='application/zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state="browser-state.json")
