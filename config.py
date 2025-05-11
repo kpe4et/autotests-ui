f9694a31b5585082645d5f62e98610e6aac1ade2
kpe4et <kpe4ets@gmail.com>
2025-05-11 15:26:20 +0300

    Настройки автотестов с Pydantic. Знакомство с pydantic-settings

---
+++ config.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from pydantic_settings import BaseSettings
+from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
+
+
+class Browser(str, Enum):
+    WEBKIT = "webkit"
+    FIREFOX = "firefox"
+    CHROMIUM = "chromium"
+
+
+class TestUser(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+
+class TestData(BaseModel):
+    image_png_file: FilePath
+
+
+class Settings(BaseSettings):
+    app_url: HttpUrl
+    headless: bool
+    browsers: list[Browser]
+    test_user: TestUser
+    test_data: TestData
+    videos_dir: DirectoryPath
+    tracing_dir: DirectoryPath
+    browser_state_file: FilePath
