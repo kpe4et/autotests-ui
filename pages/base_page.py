a40e9d1135e38e89d95186411606805ea980da42
kpe4et <kpe4ets@gmail.com>
2025-05-13 22:12:56 +0300

    Логирование взаимодействий со страницей. Практика работы с логированием

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,10 @@ from typing import Pattern
 from playwright.sync_api import Page, expect
 import allure
 
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
+
 
 class BasePage:
     # Конструктор класса, принимающий объект Page
@@ -10,13 +14,22 @@ class BasePage:
         self.page = page # Присваиваем объект page атрибуту класса
 
     def visit(self, url: str):
-        with allure.step(f'Opening the url "{url}"'):
+        step = f'Opening the url "{url}"'
+
+        with allure.step(step):
+            logger.info(step)
             self.page.goto(url, wait_until="networkidle") # Переходим по указанному URL и ждем, пока все сетевые запросы завершатся
 
     def reload(self):
-        with allure.step(f'Reloading page with url "{self.page.url}"'):
+        step = f'Reloading page with url "{self.page.url}"'
+
+        with allure.step(step):
+            logger.info(step)
             self.page.reload(wait_until="domcontentloaded") # Перезагружаем страницу и ждем, пока загрузится содержимое документа
 
     def check_current_url(self, expected_url: Pattern[str]):
-        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
+        step = f'Checking current url against "{expected_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
             expect(self.page).to_have_url(expected_url)
