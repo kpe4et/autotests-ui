a27ff185c7397e8778425461fd2682369b47cb88
kpe4et <kpe4et@users.noreply.github.com>
2025-05-17 20:47:58 +0000

    deploy: 9f584d077f6ba93083b79397731fa178c02cc47d

--- conftest.py
+++
@@ -1,5 +0,0 @@
-pytest_plugins = [
-    "fixtures.pages", # Подключаем фикстуры страниц
-    "fixtures.allure", # Подключаем фикстуры для Allure
-    "fixtures.browsers" # Подключаем фикстуры браузеров
-]
--- tools/routes.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class AppRoute(str, Enum):
-    LOGIN = "./#/auth/login"
-    REGISTRATION = "./#/auth/registration"
-    DASHBOARD = "./#/dashboard"
-    COURSES_LIST = "./#/courses"
-    CREATE_COURSE = "./#/courses/create"
-    # Добавьте все остальные маршруты
